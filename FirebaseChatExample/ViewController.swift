7d68d8da60c1135265a0716ef3fbe68fd53349f1
holejoek <eggman12363@gmail.com>
2022-01-24 14:08:26 +0300

    Initial Commit

---
+++ FirebaseChatExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChatExample
+//
+//  Created by Иван Тиминский on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
